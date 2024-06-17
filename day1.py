34f61123877701d4042875d9b2bbb27e75dbdb24
EC2 Default User <ec2-user@ip-10-16-10-21.us-west-2.compute.internal>
2024-06-17 15:50:01 +0000

    day1.code

---
+++ day1.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+str="mannual"
+print(type(str))  #type of datatype
+print(str)
+
+list=[1,2,3,4,5,6,7,8,9,10,"mango","s"]  #list
+print(list)
+print(list[9])# indexing
+print(list[1:7])#slicing
+
+input1=int(input("Enter the value 1:"))
+input2=int(input("Enter the value 2:"))
+print(input1*input2)
