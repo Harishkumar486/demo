e075a9e096a8adb550161cd55e5944cc74a5dea5
harishkumar486 <mharishkumar192003@gmail.com>
2024-06-29 06:14:32 +0000

    first commit

--- mini/day2.py
+++ mini/day2.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 
 
-app= Flask(_name_)
+app= Flask(__name__)
 
 @app.route("/",methods=['POST','GET'])
 def home():
@@ -48,5 +48,5 @@ def signup():
 
 
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(debug=True, host="0.0.0.0")
